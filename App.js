966b2b02f9de43be40a3a5240bad8493e5382cdf
sriasih <37163417+sriasih@users.noreply.github.com>
2018-03-23 18:26:58 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BelajarLayout from './src/BelajarLayout';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <BelajarLayout />
+    );
+  }
+}
